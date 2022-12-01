8c9621ba6289e8bb259997e12a8d18e14439c811
Anca Enache <ancamihaela91@yahoo.com>
2022-12-01 02:13:19 +0200

    Task 3 added

--- src/main/java/com/luxoft/bankapp/service/demo/DemoBankInitializationService.java
+++ src/main/java/com/luxoft/bankapp/service/demo/DemoBankInitializationService.java
@@ -2,6 +2,7 @@ package com.luxoft.bankapp.service.demo;
 
 import com.luxoft.bankapp.service.feed.BankFeedService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -15,9 +16,15 @@ public class DemoBankInitializationService implements BankInitializationService 
     @Autowired
     private BankFeedService feedService;
 
+    @Value("${feed.filename}")
+    private String fileName;
+
     @PostConstruct
     @Override
     public void createClientsForDemo() {
-        feedService.loadFeed(new File("demo.feed"));
+        if (fileName == null) {
+            fileName = "DEMO_FEED_FILE_NAME";
+        }
+        feedService.loadFeed(new File(fileName));
     }
 }
